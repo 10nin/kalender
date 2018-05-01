0790042b0ac22531f3786aee0352f71c4988420b
Akira Ota 10nin <akira19870221@gmail.com>
2018-05-01 15:52:07 +0900

    Update tables DDL.

--- database/kalendar_db_dump.sql
+++ database/kalendar_db_dump.sql
@@ -64,6 +64,84 @@ SET default_tablespace = '';
 SET default_with_oids = false;
 
 --
+-- Name: group_master; Type: TABLE; Schema: kalendar; Owner: postgres
+--
+
+CREATE TABLE kalendar.group_master (
+    id integer NOT NULL,
+    groupname character varying(200) NOT NULL,
+    createdon timestamp without time zone DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    createdby character varying(50) DEFAULT 'SYSTEM'::character varying NOT NULL,
+    lastupdateon timestamp without time zone DEFAULT CURRENT_TIMESTAMP,
+    lastupdateby character varying(50) DEFAULT 'SYSTEM'::character varying
+);
+
+
+ALTER TABLE kalendar.group_master OWNER TO postgres;
+
+--
+-- Name: group_master_id_seq; Type: SEQUENCE; Schema: kalendar; Owner: postgres
+--
+
+CREATE SEQUENCE kalendar.group_master_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE kalendar.group_master_id_seq OWNER TO postgres;
+
+--
+-- Name: group_master_id_seq; Type: SEQUENCE OWNED BY; Schema: kalendar; Owner: postgres
+--
+
+ALTER SEQUENCE kalendar.group_master_id_seq OWNED BY kalendar.group_master.id;
+
+
+--
+-- Name: login_infromation_master; Type: TABLE; Schema: kalendar; Owner: postgres
+--
+
+CREATE TABLE kalendar.login_infromation_master (
+    id integer NOT NULL,
+    groupid integer NOT NULL,
+    passwordhash character varying(200) NOT NULL,
+    passwordsalt character varying(200),
+    createdon timestamp without time zone DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    createdby character varying(50) DEFAULT 'SYSTEM'::character varying NOT NULL,
+    lastupdateon timestamp without time zone DEFAULT CURRENT_TIMESTAMP,
+    lastupdateby character varying(50) DEFAULT 'SYSTEM'::character varying
+);
+
+
+ALTER TABLE kalendar.login_infromation_master OWNER TO postgres;
+
+--
+-- Name: login_infromation_master_id_seq; Type: SEQUENCE; Schema: kalendar; Owner: postgres
+--
+
+CREATE SEQUENCE kalendar.login_infromation_master_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE kalendar.login_infromation_master_id_seq OWNER TO postgres;
+
+--
+-- Name: login_infromation_master_id_seq; Type: SEQUENCE OWNED BY; Schema: kalendar; Owner: postgres
+--
+
+ALTER SEQUENCE kalendar.login_infromation_master_id_seq OWNED BY kalendar.login_infromation_master.id;
+
+
+--
 -- Name: zoo_master; Type: TABLE; Schema: kalendar; Owner: postgres
 --
 
@@ -102,10 +180,40 @@ ALTER SEQUENCE kalendar.zoo_master_id_seq OWNED BY kalendar.zoo_master.id;
 
 
 --
+-- Name: group_master id; Type: DEFAULT; Schema: kalendar; Owner: postgres
+--
+
+ALTER TABLE ONLY kalendar.group_master ALTER COLUMN id SET DEFAULT nextval('kalendar.group_master_id_seq'::regclass);
+
+
+--
+-- Name: login_infromation_master id; Type: DEFAULT; Schema: kalendar; Owner: postgres
+--
+
+ALTER TABLE ONLY kalendar.login_infromation_master ALTER COLUMN id SET DEFAULT nextval('kalendar.login_infromation_master_id_seq'::regclass);
+
+
+--
 -- Name: zoo_master id; Type: DEFAULT; Schema: kalendar; Owner: postgres
 --
 
 ALTER TABLE ONLY kalendar.zoo_master ALTER COLUMN id SET DEFAULT nextval('kalendar.zoo_master_id_seq'::regclass);
+
+
+--
+-- Data for Name: group_master; Type: TABLE DATA; Schema: kalendar; Owner: postgres
+--
+
+COPY kalendar.group_master (id, groupname, createdon, createdby, lastupdateon, lastupdateby) FROM stdin;
+\.
+
+
+--
+-- Data for Name: login_infromation_master; Type: TABLE DATA; Schema: kalendar; Owner: postgres
+--
+
+COPY kalendar.login_infromation_master (id, groupid, passwordhash, passwordsalt, createdon, createdby, lastupdateon, lastupdateby) FROM stdin;
+\.
 
 
 --
@@ -117,10 +225,40 @@ COPY kalendar.zoo_master (id, zooname, createdon, createdby, latestupdateon, lat
 
 
 --
+-- Name: group_master_id_seq; Type: SEQUENCE SET; Schema: kalendar; Owner: postgres
+--
+
+SELECT pg_catalog.setval('kalendar.group_master_id_seq', 1, false);
+
+
+--
+-- Name: login_infromation_master_id_seq; Type: SEQUENCE SET; Schema: kalendar; Owner: postgres
+--
+
+SELECT pg_catalog.setval('kalendar.login_infromation_master_id_seq', 1, false);
+
+
+--
 -- Name: zoo_master_id_seq; Type: SEQUENCE SET; Schema: kalendar; Owner: postgres
 --
 
 SELECT pg_catalog.setval('kalendar.zoo_master_id_seq', 1, false);
+
+
+--
+-- Name: group_master group_master_pkey; Type: CONSTRAINT; Schema: kalendar; Owner: postgres
+--
+
+ALTER TABLE ONLY kalendar.group_master
+    ADD CONSTRAINT group_master_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: login_infromation_master login_infromation_master_pkey; Type: CONSTRAINT; Schema: kalendar; Owner: postgres
+--
+
+ALTER TABLE ONLY kalendar.login_infromation_master
+    ADD CONSTRAINT login_infromation_master_pkey PRIMARY KEY (id);
 
 
 --

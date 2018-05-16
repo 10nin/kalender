b3689f523d803ea9c3a3dc7def9eb4953ed31a9d
Akira Ota 10nin <akira19870221@gmail.com>
2018-05-16 22:42:19 +0900

    Add simple query db connection.

--- main/controllers.py
+++ main/controllers.py
@@ -3,4 +3,13 @@ from main.query import Query
 
 
 class Controller:
-    pass
+    def __init__(self):
+        self.db = Query(db_path="")
+
+    def call_get_all_groups(self):
+        return self.db.get_all_groups()
+
+
+if __name__ == "__main__":
+    c = Controller()
+    print(c.call_get_all_groups())

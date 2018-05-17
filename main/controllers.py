9d39aec66cad25230244d85a4ceb2e60ba0a0821
Akira Ota 10nin <akira19870221@gmail.com>
2018-05-17 19:00:16 +0900

    read setup from configuration file.

--- main/controllers.py
+++ main/controllers.py
@@ -1,15 +1,19 @@
 # coding=utf-8
+import os, configparser
 from main.query import Query
 
 
 class Controller:
-    def __init__(self):
-        self.db = Query(db_path="")
+    def __init__(self, config_filepath):
+        cfg = configparser.ConfigParser()
+        cfg.read(config_filepath, encoding="UTF-8")
+        url = cfg["DATABASE"]["url"]
+        self.db = Query(db_path=url)
 
     def call_get_all_groups(self):
         return self.db.get_all_groups()
 
 
 if __name__ == "__main__":
-    c = Controller()
+    c = Controller("setup.cfg")
     print(c.call_get_all_groups())

1def8ad17ae109831669e76d9d873dc59bc8826d
Akira Ota 10nin <akira19870221@gmail.com>
2018-05-19 08:03:52 +0900

    Fix query session bugs.

--- main/query.py
+++ main/query.py
@@ -14,48 +14,62 @@ class Query:
         self.engine = create_engine(db_path, echo=True)
         self.SessionClass = sessionmaker(bind=self.engine)
 
-    def insert_general(self, obj: object) -> (bool, Exception):
+    @classmethod
+    def _call_in_transaction(cls, s, fn, obj) -> (bool, Exception):
+        if obj is None:
+            return False, ValueError()
+
         try:
-            _s = self.SessionClass()
-            _s.add(obj)
-            _s.flush()
-            _s.commit()
+            fn(obj)
+            s.flush()
+            s.commit()
+            s.close()
         except Exception as e:
             return False, e
         return True,None
+
+    def delete_general(self, obj: object) -> (bool, Exception):
+        _s = self.SessionClass()
+        return Query._call_in_transaction(_s, _s.delete, obj)
+
+    def insert_general(self, obj: object) -> (bool, Exception):
+        _s = self.SessionClass()
+        return Query._call_in_transaction(_s, _s.add, obj)
 
     def insert_all(self, objects: object) -> (bool, Exception):
         """Insert 'objects' to database.
         :arg objects: expected target table instance object
         :return (True, None) is returned when success the insert, otherwise return (False, exception object)"""
-        try:
-            _s = self.SessionClass()
-            _s.add_all(objects)
-            _s.flush()
-            _s.commit()
-        except Exception as e:
-            return False, e
-        return True, None
+        _s = self.SessionClass()
+        return Query._call_in_transaction(_s, _s.add, objects)
 
     def get_salt(self, gid: int) -> str:
         """return the salt value of gid."""
         _s = self.SessionClass()
-        return _s.query(Login_Information_Master.PasswordSalt).filter(Login_Information_Master.GroupId == gid).first()
+        salt = _s.query(Login_Information_Master.PasswordSalt).filter(Login_Information_Master.GroupId == gid).first()
+        _s.close()
+        return salt
 
     def get_passwordhash(self, gid: int) -> str:
         """get password hash value from database."""
         _s = self.SessionClass()
-        return _s.query(Login_Information_Master.PasswordHash).filter(Login_Information_Master.GroupId == gid).first()
+        hs =_s.query(Login_Information_Master.PasswordHash).filter(Login_Information_Master.GroupId == gid).first()
+        hs.close()
+        return hs
 
-    def get_group(self, gid: int) -> Group_Master:
-        """get a group of match of gid."""
-        _s = self.SessionClass()
-        return _s.query(Group_Master.id, Group_Master.groupname).filter(Group_Master.id == gid).first()
-
-    def get_group_by_group_name(self, group_name: str) -> Group_Master:
+    def get_groups_by_group_name(self, group_name: str) -> Group_Master:
         """get groups of match of group_name."""
         _s = self.SessionClass()
-        return _s.query(Group_Master.id, Group_Master.groupname).filter(Group_Master.groupname == group_name).all()
+        g = _s.query(Group_Master).filter(Group_Master.groupname == group_name).all()
+        _s.close()
+        return g
+
+    def get_group(self, group_code: str) -> Group_Master:
+        """get group by unique code."""
+        _s = self.SessionClass()
+        g = _s.query(Group_Master).filter(Group_Master.groupcode == group_code).first()
+        _s.close()
+        return g
 
     def _login_registration(self, login_info):
         ret = self.insert_general(login_info)
@@ -65,20 +79,22 @@ class Query:
         else:
             return True
 
-    def group_login_registration(self, gid: int, passwd: str) -> bool:
-        g = self.get_group(gid)
+    def group_login_registration(self, group_code: str, passwd: str) -> bool:
+        g = self.get_group(group_code=group_code)
         if g is not None:
             # group is already exists
             return False
         else:
             salt = utils.get_unique_str(len(Login_Information_Master.PasswordSalt))
             hash_code = utils.get_hashval(passwd, salt)
-            info = Login_Information_Master(gid=gid, passwd_hs=hash_code, salt=salt)
+            info = Login_Information_Master(gid=g.id, passwd_hs=hash_code, salt=salt)
             return self._login_registration(info)
 
     def get_zoo(self, zid: int) -> Zoo_Master:
         _s = self.SessionClass()
-        return _s.query(Zoo_Master.id, Zoo_Master.ZooName).filter(Zoo_Master.id == zid).all()
+        z = _s.query(Zoo_Master).filter(Zoo_Master.id == zid).all()
+        _s.close()
+        return z
 
     def set_zoo_calendar(self, zid: int, opening: DateTime, closing: DateTime):
         if self.get_zoo(zid) is not None:
@@ -88,37 +104,47 @@ class Query:
 
     def get_zoo_calendar(self, calid):
         _s = self.SessionClass()
-        return _s.query(Zoo_Calendar_Master.id,
-                        Zoo_Calendar_Master.ZooMasterId,
-                        Zoo_Calendar_Master.OpeningDateTime,
-                        Zoo_Calendar_Master.ClosingDateTime).filter(Zoo_Calendar_Master.id == calid).all()
+        cal = _s.query(Zoo_Calendar_Master.id,
+                       Zoo_Calendar_Master.ZooMasterId,
+                       Zoo_Calendar_Master.OpeningDateTime,
+                       Zoo_Calendar_Master.ClosingDateTime).filter(Zoo_Calendar_Master.id == calid).all()
+        _s.close()
+        return cal
 
     def get_group_calendar(self, gid: int, fromday, today):
         _s = self.SessionClass()
-        return _s.query(Zoo_Calendar_Master.OpeningDateTime,
+        cal = _s.query(Zoo_Calendar_Master.OpeningDateTime,
                         Zoo_Calendar_Master.ClosingDateTime)\
-            .filter(Group_Calendar.ZooCalendarId == Zoo_Calendar_Master.id
-                    and Group_Calendar.GroupId == gid)\
-            .filter(Zoo_Calendar_Master.OpeningDateTime <= fromday
-                    and today <= Zoo_Calendar_Master.ClosingDateTime)\
-            .all()
+                .filter(Group_Calendar.ZooCalendarId == Zoo_Calendar_Master.id
+                        and Group_Calendar.GroupId == gid)\
+                .filter(Zoo_Calendar_Master.OpeningDateTime <= fromday
+                        and today <= Zoo_Calendar_Master.ClosingDateTime)\
+                .all()
+        _s.close()
+        return cal
 
-    def set_group_calendar(self, calid: int, gid: int):
+    def set_group_calendar(self, calid: int, group_code: str):
         """insert date time of group activity to GROUP_CALENDAR table."""
-        if (self.get_group(gid) is not None) and (self.get_zoo_calendar(calid) is not None):
-            c = Group_Calendar(gid, calid)
+        g = self.get_group(group_code)
+        if (g is not None) and (self.get_zoo_calendar(calid) is not None):
+            c = Group_Calendar(g.id, calid)
             return self.insert_general(c)[0]
         return False
 
     def get_all_zoo(self):
         """get all zoo from ZOO_MASTER table."""
         _s = self.SessionClass()
-        return _s.query(Zoo_Master.id, Zoo_Master.ZooName).all()
+        z = _s.query(Zoo_Master).all()
+        _s.close()
+        return z
 
     def get_all_groups(self):
         """get all groups from GROUP_MASTER table."""
         _s = self.SessionClass()
-        return _s.query(Group_Master.id, Group_Master.groupname).all()
+        g = s.query(Group_Master).all()
+        _s.close()
+        return g
 
-    def get_column_length(self, column):
+    @classmethod
+    def get_column_length(cls, column):
         return column.type.length
